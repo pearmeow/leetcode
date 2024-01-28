cf1e0b5389e5e21015da10f54197a3073e14c0e9
Perry <perryhuang2015@gmail.com>
2024-01-28 14:52:48 -0500

    hi

--- medium/2.cpp
+++ medium/2.cpp
@@ -1,7 +1,26 @@
 /*
 2. Add Two Numbers
 Description of problem:
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 */
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+}
 
 int main(int argc, char* argv[]) {
     return 0;
--- test.cpp
+++ test.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    std::unordered_map<int, int> test;
-    test[0] = 90;
-    std::cout << test[0] << std::endl;
-    std::cout << test[1] << std::endl;
-    std::cout << test[1] << std::endl;
-    return 0;
+    int x = 10;
+    int* y = &x;
+    std::cout << *y << std::endl;
 }
