beb93f4e112bfb8a5695e627eb20280184ca8a44
Perry <perryhuang2015@gmail.com>
2024-01-31 08:49:38 -0500

    3 done

--- medium/3.cpp
+++ medium/3.cpp
@@ -1,8 +1,31 @@
 /*
 3. Longest Substring Without Repeating Characters
 Description of problem:
+Given a string s, find the length of the longest 
+substring without repeating characters.
 */
 
+#include <string>
+#include <unordered_map>
+#include <iostream> //for testing
+
+int lengthOfLongestSubstring(std::string s) {
+    std::unordered_map<char, int> charPositions;
+    int longestSub = 0;
+    int offset = 0;
+    for (int pos = 0; pos < s.size(); ++pos) {  //iterate through each character in s
+        int charPos = charPositions[s[pos]];    //see if the current character exists
+        if (charPos != 0) {                     //if character exists in the substring
+            offset = std::max(offset, charPos); //offset to right in front of that char if it's greater
+        }
+        charPositions[s[pos]] = pos + 1;        //set the position to the current one
+        longestSub = std::max(longestSub, (pos + 1 - offset)); //update length
+    }
+    return longestSub;
+}
+
 int main(int argc, char* argv[]) {
+    std::string sample = "bacdefghijab";        //expecting 10
+    std::cout << lengthOfLongestSubstring(sample);
     return 0;
 }
