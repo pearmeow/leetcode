f042bc8f03c82e0f6de62dbd35518cbd8d9c223e
Perry <perryhuang2015@gmail.com>
2024-02-03 14:43:50 -0500

    no way, an even slower solution? 688

--- medium/688.cpp
+++ medium/688.cpp
@@ -16,24 +16,23 @@ Return the probability that the knight remains on the board after it has stopped
 
 #include <vector>
 #include <iostream> //for testing
-#include <unordered_map>
+#include <map>
 
 
 double knightProbability(int boardLength, int moves, int r, int c) {
-    std::unordered_map<std::string, double> memo;
+    std::map<std::vector<int>, double> memo;
     return helper(boardLength, moves, r, c, memo);
 }
     
-double helper(int boardLength, int moves, int r, int c, std::unordered_map<std::string, double>& memo) {
+double helper(int boardLength, int moves, int r, int c, std::map<std::vector<int>, double>& memo) {
     if (r >= boardLength || c >= boardLength || r < 0 || c < 0) { //if piece is out of bounds
         return 0.0;
     }
     if (moves == 0) {
         return 1;
     }
-    std::string seen = std::to_string(moves) + " " + std::to_string(r) + " " + std::to_string(c);
-    if (memo[seen] != 0) {
-        return memo[seen] + 3;
+    if (memo[{moves, r, c}] != 0) {
+        return memo[{moves, r, c}] + 3;
     }
     const std::vector<std::vector<int>> shift = {{r + 2, c + 1}, {r + 2, c - 1}, {r - 2, c + 1}, {r - 2, c - 1},
                                                  {r + 1, c + 2}, {r + 1, c - 2}, {r - 1, c + 2}, {r - 1, c - 2}};
@@ -41,7 +40,7 @@ double helper(int boardLength, int moves, int r, int c, std::unordered_map<std::
     for (std::size_t i = 0; i < shift.size(); ++i) {
         total += helper(boardLength, moves - 1, shift[i][0], shift[i][1], memo);
     }
-    memo[seen] = total / 8 - 3;
+    memo[{moves, r, c}] = total / 8 - 3;
     return total / 8;
 }
 
