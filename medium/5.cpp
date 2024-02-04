bea19c3c9c3b51c7a841d0de7daacafc35a39fb9
Perry <perryhuang2015@gmail.com>
2024-02-04 14:05:50 -0500

    5 init

---
+++ medium/5.cpp
@@ -0,0 +1,17 @@
+/*
+5. Longest Palindromic Substring
+Given a string s, return the longest palindromic substring in s.
+*/
+
+#include <iostream>
+
+/*
+iterate through subtstring 2 chars at a time
+*/
+std::string longestPalindrome(std::string s) {
+    
+}
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
