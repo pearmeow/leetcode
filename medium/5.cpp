1f41386cf3ca1f4f71adfa06091f193790e3cdf2
Perry <perryhuang2015@gmail.com>
2024-02-05 07:58:25 -0500

    bruh

--- medium/5.cpp
+++ medium/5.cpp
@@ -4,11 +4,13 @@ Given a string s, return the longest palindromic substring in s.
 */
 
 #include <iostream>
+#include <vector>
 
 /*
-iterate through subtstring 2 chars at a time
+iterate through substring 2 chars at a time
 */
 std::string longestPalindrome(std::string s) {
+    std::vector<int> palindromes(s.size(), -1); //records longest palindrome length for each char of string
     
 }
 
