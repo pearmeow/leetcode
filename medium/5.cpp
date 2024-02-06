b32ac942b0cde006c49ce1f5a0f4160d95d4a2cc
Perry <perryhuang2015@gmail.com>
2024-02-06 16:52:42 -0500

    easy question 9

--- medium/5.cpp
+++ medium/5.cpp
@@ -16,6 +16,9 @@ std::string longestPalindrome(std::string s) {
     int longest = 0;
     for (size_t i = 0; i < s.size() - 1; ++i) {
         if (s[i] == s[i + 1]) {
+
+        }
+        else {
             
         }
     }
