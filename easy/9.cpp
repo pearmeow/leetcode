d7e43176e23c5a505877b05313da51b603c483f1
Perry <perryhuang2015@gmail.com>
2024-02-07 08:07:32 -0500

    dying

---
+++ easy/9.cpp
@@ -0,0 +1,27 @@
+/*
+@file : 9.cpp
+@author : Perry Huang
+@brief : is the number a palindrome
+*/
+
+#include <iostream>
+
+bool isPalindrome(int x) {
+    if (x < 0) {return false;}
+    std::size_t rvrs = 0, count = 0, orig = x;
+    while (orig) {
+        rvrs *= 10;
+        rvrs += orig % 10;
+        orig /= 10;
+    }
+    while (x) {
+        if (!(rvrs % 10 == x % 10)) {return false;}
+        rvrs /= 10;
+        x /= 10;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
---
+++ medium/6.cpp
@@ -0,0 +1,22 @@
+/*
+@file : 6.cpp
+@author : Perry Huang
+@brief Zigzag Conversion
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+*/
+
+#include <string>
+#include <vector>
+
+std::string convert(std::string s, int numRows) {
+    std::vector<std::vector<char>> chars;
+    char buf;
+    while (buf << s) {
+
+    }
+}
