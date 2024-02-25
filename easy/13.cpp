47eb6452ccd0121e2e1e7cd66f2c12d2fc0e9793
Perry <perryhuang2015@gmail.com>
2024-02-25 14:48:35 -0500

    init 13 and fix 12

---
+++ easy/13.cpp
@@ -0,0 +1,7 @@
+/*
+@file: 13.cpp
+@author: Perry Huang
+@date: 2/25/2024
+
+
+*/
--- medium/12.cpp
+++ medium/12.cpp
@@ -11,7 +11,7 @@ Given an integer, convert it to a roman numeral.
 #include <unordered_map>
 
 std::string intToRoman(int num) {
-  std::string romanNumeral;
+  std::string romNum;
   std::unordered_map<int, std::string> values = {{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, 
                                                 {10, "X"}, {40, "XL"}, {50, "L"}, 
                                                 {90, "XC"}, {100, "C"}, {400, "CD"}, 
@@ -21,9 +21,9 @@ std::string intToRoman(int num) {
   while (num != 0) {
     if (nums[pos] > num) --pos;
     else {
-      romanNumeral += values[nums[pos]];
+      romNum += values[nums[pos]];
       num -= nums[pos];
     }
   }
-  return romanNumeral;
+  return romNum;
 }
