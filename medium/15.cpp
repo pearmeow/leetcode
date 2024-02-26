9cf7f07a1f77cf3dc7ccf7f70a99d28d88a73908
Perry <perryhuang2015@gmail.com>
2024-02-26 10:19:01 -0500

    edit init 15

--- medium/15.cpp
+++ medium/15.cpp
@@ -4,8 +4,10 @@
 @date: 2/26/2024
 
 15. 3Sum
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] 
-such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Given an integer array nums, return all the triplets
+[nums[i], nums[j], nums[k]] such that
+i != j, i != k, and j != k
+nums[i] + nums[j] + nums[k] == 0.
 
 Notice that the solution set must not contain duplicate triplets.
 */
@@ -13,7 +15,7 @@ Notice that the solution set must not contain duplicate triplets.
 #include <vector>
 
 std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-  
+
 }
 
 int main() {
