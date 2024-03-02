a26baebbf00ef981dda96cb8faa8fc61094066db
Perry <perryhuang2015@gmail.com>
2024-03-02 12:27:53 -0500

    70 done, ez fibonacci

---
+++ easy/70.cpp
@@ -0,0 +1,24 @@
+/*
+@file: 70.cpp
+@author: Perry Huang
+@date: 3/2/2024
+
+70. Climbing Stairs
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+
+#include <vector>
+#include <iostream>
+
+int climbStairs(int n) {
+  std::vector<int> ways = {1, 2};
+  for (int i = 2; i < n; ++i) {
+    ways.push_back(ways[i - 1] + ways[i - 2]);
+  }
+  return ways[n - 1];
+}
+
+int main() {
+  std::cout << climbStairs(13) << std::endl;
+}
