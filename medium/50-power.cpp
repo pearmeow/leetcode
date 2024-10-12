f78ae513b978bfbe20e9a5af336309bf77f8d217
pearmeow <perryhuang2015@gmail.com>
2024-10-12 14:42:33 -0400

    redid 50

---
+++ medium/50-power.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file 50-power.cpp
+ * @author Perry Huang
+ * @date 2024-10-12
+ * 50. Pow(x, n)
+ * Calculate x raised to the power of n
+ */
+
+#include <cmath>
+
+double myPow(double x, int n) {
+  if (n == 0) return 1;
+  if (n == 1) return x;
+  if (n == -1) return 1 / x;
+  double halfPow = myPow(x, std::abs(n) / 2);
+  double answer = halfPow * halfPow;
+  if (std::abs(n) % 2 == 1) answer *= x;
+  if (n < 0) return 1 / answer;
+  return answer;
+}
