db26d8c426123c835b52fa43b7fc2c5df24357b3
pearmeow <perryhuang2015@gmail.com>
2024-10-28 21:37:39 -0400

    done with 121

--- easy/121-buy-stock.cpp
+++ easy/121-buy-stock.cpp
@@ -2,11 +2,20 @@
  * @file 121-buy-stock.cpp
  * @author Perry Huang
  * @date 2024-10-28
+ * 121. Best Time to Buy and Sell Stock
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
  */
 
 #include <vector>
 
 int maxProfit(std::vector<int>& prices) {
-
-  return 0;
+  int profit = 0, minimum = prices[0];
+  for (size_t i = 0; i < prices.size(); ++i) {
+    profit = std::max(prices[i] - minimum, profit);
+    if (prices[i] < minimum) minimum = prices[i];
+  }
+  return profit;
 }
