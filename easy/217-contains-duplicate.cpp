b812c2cb314f16bc63a5c628fc0b4ecb4a03521e
pearmeow <perryhuang2015@gmail.com>
2025-01-09 20:12:24 -0500

    217

---
+++ easy/217-contains-duplicate.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file 217-contains-duplicate.cpp
+ * @author Perry Huang
+ * @date 2025-01-09
+ * 217. Contains Duplicate
+ * Given an integer array nums, return true if any value appears at least twice in the array, 
+ * and return false if every element is distinct.
+ */
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+bool containsDuplicate(std::vector<int>& nums) {
+  std::unordered_set<int> set;
+  for (size_t i = 0; i < nums.size(); ++i) {
+    if (set.find(nums[i]) != set.end()) {
+      return true;
+    }
+    set.insert(nums[i]);
+  }
+  return false;
+}
+
+int main() {
+  std::vector<int> testVec = {1, 2, 3, 4, 1};
+  if (containsDuplicate(testVec)) {
+    std::cout << "There exists a duplicate\n";
+  } else {
+    std::cout << "There is no duplicate\n";
+  }
+  return 0;
+}
