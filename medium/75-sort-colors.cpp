5a1848354f80dd084c64feafc0394b24ff20fbe2
pearmeow <perryhuang2015@gmail.com>
2025-05-16 22:03:39 -0400

    tried one pass

---
+++ medium/75-sort-colors.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file 75-sort-colors.cpp
+ * @author Perry Huang
+ * @date 2025-05-16
+ * 75. Sort Colors
+ * Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color 
+ * are adjacent, with the colors in the order red, white, and blue.
+ * We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+ * You must solve this problem without using the library's sort function.
+ */
+
+#include <vector>
+#include <utility>
+
+void sortColors(std::vector<int>& nums) {
+  if (nums.size() == 0 || nums.size() == 1) return;
+  size_t back = nums.size() - 1;
+  size_t front = 0;
+  while (nums[back] == 2 && back != 0) --back;
+  while (nums[front] == 0 && front < nums.size()) ++front;
+  size_t curr = front;
+  while (curr <= back) {
+    if (nums[curr] == 2) {
+      std::swap(nums[curr], nums[back]);
+      --back;
+      if (nums[curr] == 0) {
+        ++front;
+      }
+    } else if (nums[curr] == 0) {
+      std::swap(nums[curr], nums[front]);
+      ++front;
+    }
+    ++curr;
+  }
+}
