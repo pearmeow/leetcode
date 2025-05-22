db534c951a4f58d61bf3d8715d38328fd91bc955
pearmeow <perryhuang2015@gmail.com>
2025-05-21 21:34:59 -0400

    Add starter code for daily question 3362

---
+++ medium/3362-zero-array-transformation-iii.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file 3362-zero-array-transformation-iii.cpp
+ * @author Perry Huang
+ * @date 2025-05-21
+ * 3362. Zero Array Transformation III
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * You are given an integer array nums of length n and a 2D array queries where queries[i] = [li, ri].
+ * Each queries[i] represents the following action on nums
+ * Decrement the value at each index in the range [li, ri] in nums by at most 1
+ * The amount by which the value is decremented can be chosen independently for each index
+ * A Zero Array is an array with all its elements equal to 0
+ * Return the maximum number of elements that can be removed from queries, such that nums can still be converted to a zero 
+ * array using the remaining queries. If it is not possible to convert nums to a zero array, return -1.
+ */
+
+#include <vector>
+int maxRemoval(std::vector<int>& nums, std::vector<std::vector<int>>& queries) {
+  
+  return -1;
+}
