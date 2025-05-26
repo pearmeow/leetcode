3a5af098655056edd9701fcb18ee8049bec8a5c4
pearmeow <perryhuang2015@gmail.com>
2025-05-26 13:54:34 -0400

    Add iterative attempt for 1857

--- hard/1857-largest-color-value-in-a-directed-graph.cpp
+++ hard/1857-largest-color-value-in-a-directed-graph.cpp
@@ -15,65 +15,45 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 
 const int COLORS = 26;
-
-int modifiedKahnTopoSort(const std::string& colors, const std::vector<std::vector<int>>& adjList,
-    std::vector<int>& indegrees, std::vector<std::vector<int>>& pathValues) {
-  std::vector<int> parents;
-  bool cyclePossible = false;
-  for (size_t i = 0; i < indegrees.size(); ++i) {
-    if (indegrees[i] == 0) {
-      parents.push_back(i);
-    } else if (indegrees[i] > 0) {
-      cyclePossible = true;
-    }
-  }
-  if (parents.size() == 0 && cyclePossible) {
-    return -1;
-  } else if (parents.size() == 0) {
-    return 0;
-  }
-  for (size_t i = 0; i < parents.size(); ++i) {
-    for (size_t child = 0; child < adjList[parents[i]].size(); ++child) {
-      --indegrees[adjList[parents[i]][child]];
-    }
-    indegrees[parents[i]] = -1; // set to -1 to prevent readding to vector in future recursions
-  }
-  int maxColor = modifiedKahnTopoSort(colors, adjList, indegrees, pathValues);
-  if (maxColor == -1) {
-    return maxColor;
-  }
-  // iterate through vector of parents and update dp table with new color values
-  // update by looping over adjacency list and looping through the vectors of the children,
-  // updating each color with the max
-  for (size_t i = 0; i < parents.size(); ++i) {
-    for (size_t child = 0; child < adjList[parents[i]].size(); ++child) {
-      for (size_t color = 0; color < COLORS; ++color) {
-        int parentMax = pathValues[parents[i]][color];
-        int childMax = pathValues[adjList[parents[i]][child]][color];
-        pathValues[parents[i]][color] = std::max(parentMax, childMax);
-      }
-    }
-    ++pathValues[parents[i]][colors[parents[i]] - 'a'];
-  }
-  for (size_t i = 0; i < parents.size(); ++i) {
-    for (size_t color = 0; color < COLORS; ++color) {
-      maxColor = std::max(pathValues[parents[i]][color], maxColor);
-    }
-  }
-  return maxColor;
-}
 
 int largestPathValue(const std::string& colors, const std::vector<std::vector<int>>& edges) {
   std::vector<std::vector<int>> adjList(colors.size());
   std::vector<int> indegrees(colors.size());
-  for (size_t i = 0; i < edges.size(); ++i) {
-    int parent = edges[i][0];
-    int child = edges[i][1];
+  for (const std::vector<int>& edge : edges) {
+    int parent = edge[0];
+    int child = edge[1];
     adjList[parent].push_back(child);
     ++indegrees[child];
   }
   std::vector<std::vector<int>> pathValues(colors.size(), std::vector<int>(COLORS));
-  return modifiedKahnTopoSort(colors, adjList, indegrees, pathValues);
+  std::queue<int> parents;
+  for (int i : indegrees) {
+    if (i == 0) {
+      parents.push(i);
+    }
+  }
+  int maxColor = 0;
+  int totalParents = 0;
+  while (parents.empty() == false) {
+    ++totalParents;
+    int currPar = parents.front();
+    parents.pop();
+    pathValues[currPar][colors[currPar] - 'a'] += 1;
+    maxColor = std::max(pathValues[currPar][colors[currPar] - 'a'], maxColor);
+    for (int child : adjList[currPar]) {
+      indegrees[child] -= 1;
+      if (indegrees[child] == 0) {
+        parents.push(child);
+      }
+      for (size_t i = 0; i < COLORS; ++i) {
+        pathValues[child][i] = std::max(pathValues[currPar][i], pathValues[child][i]);
+        maxColor = std::max(pathValues[child][i], maxColor);
+      }
+    }
+  }
+  if (totalParents != colors.size()) return -1;
+  return maxColor;
 }
