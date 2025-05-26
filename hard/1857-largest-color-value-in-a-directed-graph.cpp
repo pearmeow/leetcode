27aa4e322b041516c4e98e4ea218304b5693d668
pearmeow <perryhuang2015@gmail.com>
2025-05-26 11:07:54 -0400

    Add incorrect solution for lc daily 1857

--- hard/1857-largest-color-value-in-a-directed-graph.cpp
+++ hard/1857-largest-color-value-in-a-directed-graph.cpp
@@ -12,9 +12,73 @@
  * frequently occurring color along that path.
  * Return the largest color value of any valid path in the given graph, or -1 if the graph contains a cycle.
  */
+
 #include <string>
 #include <vector>
-#include <queue>
+#include <iostream>
+
+const int COLORS = 26;
+
+// recursively finds out if a cycle exists but also updates dp table
+// will take in colors to track the colors
+// will take in adjacency list to subtract indegrees
+// will take in indegrees to track indegrees
+// will take in dp table to update it
+// returns -1 if a cycle is detected
+// otherwise returns the largest color value
+int modifiedKahnTopoSort(const std::string& colors, const std::vector<std::vector<int>>& adjList,
+    std::vector<int>& indegrees, std::vector<std::vector<int>>& pathValues) {
+  std::cout << "This is a recursive call. There should only be as many of these as the longest path\n";
+  // push back vertices with zero to vector
+  std::vector<int> parents;
+  bool cyclePossible = false;
+  std::cout << "Indegrees: ";
+  for (size_t i = 0; i < indegrees.size(); ++i) {
+    std::cout << indegrees[i] << " ";
+    if (indegrees[i] == 0) {
+      parents.push_back(i);
+    } else if (indegrees[i] > 0) {
+      cyclePossible = true;
+    }
+  }
+  std::cout << std::endl;
+  // if no vertices are less than 0 then we have a cycle, return -1
+  if (parents.size() == 0 && cyclePossible) {
+    std::cout << "The parent size 0 and possibility of cycle are both true\n";
+    return -1;
+  }
+  // iterate through vector and subtract indegrees of child vertices
+  // no need to "dequeue" parents because the "queue" is a vector created freshly every recursive case
+  for (size_t parent = 0; parent < parents.size(); ++parent) {
+    for (size_t child = 0; child < adjList[parent].size(); ++child) {
+      --indegrees[adjList[parent][child]];
+    }
+    indegrees[parents[parent]] = -1; // set to -1 to prevent readding to vector in future recursions
+  }
+  // recursively call yourself with the same arguments
+  int maxColor = modifiedKahnTopoSort(colors, adjList, indegrees, pathValues);
+  if (maxColor == -1) {
+    std::cout << "The recursive call has returned -1\n";
+    return maxColor;
+  }
+  // iterate through vector of parents and update dp table with new color values
+  // update by looping over adjacency list and looping through the vectors of the children,
+  // updating each color with the max
+  for (size_t parent = 0; parent < parents.size(); ++parent) {
+    for (size_t child = 0; child < adjList[parent].size(); ++child) {
+      for (size_t color = 0; color < COLORS; ++color) {
+        pathValues[parent][color] = std::max(pathValues[parent][color], pathValues[child][color]);
+      }
+    }
+    ++pathValues[parent][colors[parent]];
+  }
+  for (size_t parent = 0; parent < parents.size(); ++parent) {
+    for (size_t color = 0; color < COLORS; ++color) {
+      maxColor = std::max(pathValues[parent][color], maxColor);
+    }
+  }
+  return maxColor;
+}
 
 int largestPathValue(const std::string& colors, std::vector<std::vector<int>>& edges) {
   std::vector<std::vector<int>> adjList(colors.size());
@@ -25,5 +89,12 @@ int largestPathValue(const std::string& colors, std::vector<std::vector<int>>& e
     adjList[parent].push_back(child);
     ++indegrees[child];
   }
-  return -1;
+  std::vector<std::vector<int>> pathValues(colors.size(), std::vector<int>(COLORS));
+  return modifiedKahnTopoSort(colors, adjList, indegrees, pathValues);
+}
+
+int main() {
+  const std::string colors = "abaca";
+  std::vector<std::vector<int>> edges ={{0, 1}, {0, 2}, {2,3}, {3,4}};
+  std::cout << largestPathValue(colors, edges) << std::endl;
 }
