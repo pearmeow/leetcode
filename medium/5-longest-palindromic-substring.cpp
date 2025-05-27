c7d35c0db2ebdc52a1edd902e44435890f2fe761
pearmeow <perryhuang2015@gmail.com>
2025-05-27 13:33:02 -0400

    Optimize solution to use less memory but still fails

--- medium/5-longest-palindromic-substring.cpp
+++ medium/5-longest-palindromic-substring.cpp
@@ -11,33 +11,34 @@
 
 void updTable(const std::string& s, size_t row, size_t col, std::vector<std::vector<std::string>>& dp) {
   if (col == 0 || col > dp.size() - 1 || row >= dp.size() - 1) return;
-  size_t nextRow = dp[row + 1][col].size();
-  size_t prevCol = dp[row][col - 1].size();
   size_t diagonal = dp[row + 1][col - 1].size();
   if (s[row] == s[col]) {
-    if ((row + 1 > col - 1 || diagonal == col - row - 1) && diagonal + 2 > nextRow && diagonal + 2 > prevCol) {
+    if (row + 1 > col - 1 || diagonal == col - row - 1) {
       dp[row][col] = s[row] + dp[row + 1][col - 1] + s[col];
     }
-  }
-  if (nextRow > prevCol) {
-    dp[row][col] = dp[row + 1][col];
-  } else {
-    dp[row][col] = dp[row][col - 1];
   }
 }
 
 std::string longestPalindrome(const std::string& s) {
   // dp[i,j] is the longest palindrome from i to j
-  std::vector<std::vector<std::string>> dp(s.size(), std::vector<std::string>(s.size(), ""));
+  std::vector<std::vector<std::string>> dp(s.size(), std::vector<std::string>(s.size()));
   for (size_t i = 0; i < dp.size(); ++i) {
-    // base case i == j and size is 1
+    // base case i == j so size is 1
     dp[i][i] = s[i];
   }
-  // traverses the table diagonally
+  // traverse table diagonally
   for (size_t offset = 1; offset < dp.size(); ++offset) {
     for (size_t row = 0, col = offset; col < dp.size(); ++row, ++col) {
       updTable(s, row, col, dp);
     }
   }
-  return dp[0][s.size() - 1];
+  std::string longest;
+  for (size_t i = 0; i < dp.size(); ++i) {
+    for (size_t j = 0; j < dp.size(); ++j) {
+      if (longest.size() < dp[i][j].size()) {
+        longest = dp[i][j];
+      }
+    }
+  }
+  return longest;
 }
