6955f07b76b7e7324d9e9624c37739ab8ed42e80
pearmeow <perryhuang2015@gmail.com>
2025-05-28 14:36:27 -0400

    Finalize O(n) runtime algo for LC Q53 Maximum Subarray

---
+++ medium/53-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file 53-maximum-subarray.cpp
+ * @author Perry Huang
+ * @date 2025-05-28
+ * 53. Maximum Subarray
+ * Given an integer array nums, find the subarray with the largest sum, and return its sum.
+ */
+
+#include <vector>
+
+int maxSubArray(std::vector<int>& nums) {
+  if (nums.size() == 0) return 0;
+  int curr = 0;
+  int maxSub = nums[0];
+  for (size_t i = 0; i < nums.size(); ++i) {
+    curr += nums[i];
+    maxSub = std::max(curr, maxSub);
+    if (curr < 0) curr = 0;
+  }
+  return maxSub;
+}
