5feb7178475960b81b75704bb8b8de25b0b5e5e4
pearmeow <perryhuang2015@gmail.com>
2025-06-07 23:03:24 -0400

    Create initial file for LC Daily 386

---
+++ medium/386-lexicographical-numbers.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file 386-lexicographical-numbers.cpp
+ * @author Perry Huang
+ * @date 2025-06-07
+ * 386. Lexicographical Numbers
+ * Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+ * You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+ */
+
+#include <vector>
+
+std::vector<int> lexicalOrder(int n) {
+    std::vector<int> res(n);
+}
