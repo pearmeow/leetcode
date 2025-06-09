2a95bf05faa9fa4b3a0fc93fc0b9206f42746b5c
pearmeow <perryhuang2015@gmail.com>
2025-06-09 09:42:23 -0400

    Finalize LC Daily 386 by looking at editorial

--- medium/386-lexicographical-numbers.cpp
+++ medium/386-lexicographical-numbers.cpp
@@ -11,4 +11,19 @@
 
 std::vector<int> lexicalOrder(int n) {
     std::vector<int> res(n);
+    int curr = 1;
+    size_t ind = 0;
+    for (int i = 0; i < n; ++i) {
+        res[ind] = curr;
+        ++ind;
+        if (curr * 10 <= n) {
+            curr *= 10;
+        } else {
+            while (curr % 10 == 9 || curr >= n) {
+                curr /= 10;
+            }
+            curr += 1;
+        }
+    }
+    return res;
 }
