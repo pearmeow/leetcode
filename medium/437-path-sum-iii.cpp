4fccb1339c8446310f690d9559c590deb8576cc5
pearmeow <perryhuang2015@gmail.com>
2025-06-08 21:45:26 -0400

    Create initial file for LC Q437 path sum

---
+++ medium/437-path-sum-iii.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file 437-path-sum-iii.cpp
+ * @author Perry Huang
+ * @date 2025-06-08
+ * 437. Path Sum III
+ * Given the root of a binary tree and an integer targetSum, return the number of paths
+ * where the sum of the values along the path equals targetSum.
+ * The path does not need to start or end at the root or a leaf, but it must go downwards
+ * (i.e., traveling only from parent nodes to child nodes).
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int pathSum(TreeNode* root, int targetSum) {
+    return 0;
+}
