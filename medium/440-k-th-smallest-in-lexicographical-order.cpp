9e8e49d25fc053d6382dacdff9087edf1419697b
pearmeow <perryhuang2015@gmail.com>
2025-06-09 09:45:41 -0400

    Create initial file for LC Daily 440

---
+++ medium/440-k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,11 @@
+/*
+ * @file 440-k-th-smallest-in-lexicographical-order.cpp
+ * @author Perry Huang
+ * @date 2025-06-09
+ * 440. K-th Smallest in Lexicographical Order
+ * Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n].
+ */
+
+int findKthNumber(int n, int k) {
+    return 0;
+}
