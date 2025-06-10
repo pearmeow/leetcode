63b9e660051b12cc835e98c22e101d86b61ce550
pearmeow <perryhuang2015@gmail.com>
2025-06-10 08:53:48 -0400

    Finalize LC Daily 3442

---
+++ easy/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,40 @@
+/*
+ * @file 3442-maximum-difference-between-even-and-odd-frequency-i.cpp
+ * @author Perry Huang
+ * @date 2025-06-09
+ * 3442. Maximum Difference Between Even and Odd Frequency I
+ * You are given a string s consisting of lowercase English letters.
+ * Your task is to find the maximum difference diff = a1 - a2 between
+ * the frequency of characters a1 and a2 in the string such that:
+ * a1 has an odd frequency in the string.
+ * a2 has an even frequency in the string.
+ * Return this maximum difference.
+ */
+
+#include <string>
+#include <vector>
+
+int maxDifference(const std::string& s) {
+    std::vector<int> freq(26);
+    for (size_t i = 0; i < s.size(); ++i) {
+        ++freq[s[i] - 'a'];
+    }
+    int evenMax = 0;
+    int evenMin = 0;
+    int oddMax = 0;
+    int oddMin = 0;
+    for (size_t i = 0; i < freq.size(); ++i) {
+        if (freq[i] % 2 == 0) {
+            evenMax = std::max(evenMax, freq[i]);
+            if (evenMin == 0 || (evenMin > freq[i] && freq[i] != 0)) {
+                evenMin = freq[i];
+            }
+        } else {
+            oddMax = std::max(oddMax, freq[i]);
+            if (oddMin == 0 || (oddMin > freq[i] && freq[i] != 0)) {
+                oddMin = freq[i];
+            }
+        }
+    }
+    return std::max(std::max(oddMax - evenMin, oddMax - evenMax), std::max(oddMin - evenMin, oddMin - evenMax));
+}
