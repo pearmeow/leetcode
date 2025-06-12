4627b8fb7e11a4dec32e30cb20063383e86d1e23
pearmeow <perryhuang2015@gmail.com>
2025-06-12 11:26:56 -0400

    Redo LC Q238 but with 1 pass and better algo

--- medium/238-product-except-self.cpp
+++ medium/238-product-except-self.cpp
@@ -1,28 +1,25 @@
 /**
- * @file: 238-product-except-self.cpp
- * @author: Perry Huang
- * @date: 3/26/2024
+ * @file 238-product-except-self.cpp
+ * @author Perry Huang
+ * @date 2025-06-12
  * 238. Product of Array Except Self
- * Given an integer array nums, return an array answer such that answer[i] 
+ * Given an integer array nums, return an array answer such that answer[i]
  * is equal to the product of all the elements of nums except nums[i].
  * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
  * You must write an algorithm that runs in O(n) time and without using the division operation.
  */
 
-#include <iostream>
 #include <vector>
 
-std::vector<int> productExceptSelf(std::vector<int>& nums) {
-    std::vector<int> ans(nums);
-    std::size_t cap = nums.size();
-    for (std::size_t i = 1; i < cap; ++i) {
-        nums[i] *= nums[i - 1];
-        ans[cap - i - 1] *= ans[cap - i];
+std::vector<int> productExceptSelf(const std::vector<int>& nums) {
+    std::vector<int> res(nums.size(), 1);
+    int prev = 1;
+    int prevReverse = 1;
+    for (std::size_t i = 1; i < nums.size(); ++i) {
+        prev *= nums[i - 1];
+        res[i] *= prev;
+        prevReverse *= nums[nums.size() - i];
+        res[nums.size() - i - 1] *= prevReverse;
     }
-    for (std::size_t i = 0; i < cap; ++i) {
-        ans[i] = 1;
-        if (i > 0) ans[i] *= nums[i - 1];
-        if (i < cap - 1) ans[i] *= ans[i + 1];
-    }
-    return ans;
+    return res;
 }
