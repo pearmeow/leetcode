9a23f183f738bfecb71498bc647f1fd187e23823
pearmeow <perryhuang2015@gmail.com>
2025-06-18 14:23:47 -0400

    Rename file of Q3

---
+++ medium/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file 3-longest-substring-without-repeating-characters.cpp
+ * @author Perry Huang
+ * @date 2025-06-18
+ * Given a string s, find the length of the longest substring without duplicate characters.
+ */
+
+#include <string>
+#include <map>
+
+int lengthOfLongestSubstring(const std::string& s) {
+    std::map<char, size_t> positions;
+    size_t maxSize = 0;
+    size_t currSize = 0;
+    size_t startPos = 0;
+    for (size_t i = 0; i < s.size(); ++i) {
+        if (positions.find(s[i]) == positions.end()) {
+            ++currSize;
+            positions[s[i]] = i;
+        } else if (positions[s[i]] < startPos) {
+            ++currSize;
+            positions[s[i]] = i;
+        } else {
+            maxSize = std::max(currSize, maxSize);
+            currSize = i - positions[s[i]];
+            startPos = positions[s[i]] + 1;
+            positions[s[i]] = i;
+        }
+    }
+    maxSize = std::max(currSize, maxSize);
+    return maxSize;
+}
