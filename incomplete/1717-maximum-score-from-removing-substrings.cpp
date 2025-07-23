8a4bf01848aeb9ea6cb7d1a8af45b9a4ff2839f9
Perry Huang <perryhuang2015@gmail.com>
2025-07-22 23:03:34 -0400

    Add Daily 1717 initial file

---
+++ incomplete/1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file 1717-maximum-score-from-removing-substrings.cpp
+ * @author Perry Huang
+ * @date 2025-07-22
+ * 1717. Maximum Score From Removing Substrings
+ * You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+ * Remove substring "ab" and gain x points.
+ * For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+ * Remove substring "ba" and gain y points.
+ * For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+ * Return the maximum points you can gain after applying the above operations on s.
+ */
+
+#include <string>
+
+int maximumGain(const std::string& s, int x, int y) {
+
+}
