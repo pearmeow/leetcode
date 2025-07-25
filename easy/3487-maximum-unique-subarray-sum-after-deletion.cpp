f5d927a22bee016d8c70653126e42b5d6a91a9fb
Perry Huang <perryhuang2015@gmail.com>
2025-07-24 21:26:49 -0400

    Finalize Daily 3487 with set

---
+++ easy/3487-maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file 3487-maximum-unique-subarray-sum-after-deletion.cpp
+ * @author Perry Huang
+ * @date 2025-07-24
+ * 3487. Maximum Unique Subarray Sum After Deletion
+ * You are given an integer array nums.
+ * You are allowed to delete any number of elements from nums without making it empty. After performing the
+ * deletions, select a subarray of nums such that: All elements in the subarray are unique. The sum of the elements
+ * in the subarray is maximized. Return the maximum sum of such a subarray.
+ */
+
+#include <vector>
+#include <unordered_set>
+
+int maxSum(const std::vector<int>& nums) {
+    int most = nums[0];
+    int sum = 0;
+    std::unordered_set<int> found;
+    for (int i : nums) {
+        if (found.find(i) == found.end()) {
+            found.insert(i);
+            if (i > 0) {
+                sum += i;
+            }
+            most = std::max(most, i);
+        }
+    }
+    if (most < 0) {
+        return most;
+    }
+    return sum;
+}
