49afceb48011290891a39c22261be02b8c36dccc
Perry Huang <perryhuang2015@gmail.com>
2025-07-27 11:30:40 -0400

    Fix up styling in neetcode exclusive

--- neetcode-exclusive/encode-and-decode-strings.cpp
+++ neetcode-exclusive/encode-and-decode-strings.cpp
@@ -11,34 +11,37 @@
 #include <string>
 #include <vector>
 
-std::string encode(const std::vector<std::string>& strs) {
-    std::string res;
-    for (const std::string& s : strs) {
-        res += std::to_string(s.size()) + "#" + s;
-    }
-    return res;
-}
-
-std::vector<std::string> decode(const std::string& s) {
-    size_t curr = 0;
-    size_t currSize = 0;
-    std::string sizeStr;
-    std::vector<std::string> res;
-    while (curr < s.size()) {
-        if (s[curr] != '#') {
-            sizeStr += s[curr];
-            ++curr;
-        } else {
-            currSize = std::stoi(sizeStr);
-            if (currSize != 0) {
-                res.push_back(s.substr(curr + 1, currSize));
-            } else {
-                res.push_back("");
-            }
-            curr += currSize + 1;
-            currSize = 0;
-            sizeStr.clear();
+class Solution {
+   public:
+    std::string encode(const std::vector<std::string>& strs) {
+        std::string res;
+        for (const std::string& s : strs) {
+            res += std::to_string(s.size()) + "#" + s;
         }
+        return res;
     }
-    return res;
-}
+
+    std::vector<std::string> decode(const std::string& s) {
+        size_t curr = 0;
+        size_t currSize = 0;
+        std::string sizeStr;
+        std::vector<std::string> res;
+        while (curr < s.size()) {
+            if (s[curr] != '#') {
+                sizeStr += s[curr];
+                ++curr;
+            } else {
+                currSize = std::stoi(sizeStr);
+                if (currSize != 0) {
+                    res.push_back(s.substr(curr + 1, currSize));
+                } else {
+                    res.push_back("");
+                }
+                curr += currSize + 1;
+                currSize = 0;
+                sizeStr.clear();
+            }
+        }
+        return res;
+    }
+};
